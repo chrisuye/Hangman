693909cd84245fcf1879a22076bf45708fc9ca6b
Christian Seyoum <chris@Christians-Air.fios-router.home>
2019-10-14 14:58:05 -0400

    Second commit

--- app/src/main/java/edu/towson/cosc431/christian/hangman/DatabaseHelper.kt
+++ app/src/main/java/edu/towson/cosc431/christian/hangman/DatabaseHelper.kt
@@ -8,27 +8,26 @@ import android.database.sqlite.SQLiteOpenHelper
 class DatabaseHelper(context: Context):SQLiteOpenHelper (context,dbname, factory, version) {
     override fun onCreate(p0: SQLiteDatabase?) {
         p0?.execSQL("create table user (id integer primary key autoincrement,"+
-                "name varchar(30), email varchar(100), password varchar(20))")
+                "name varchar(30), password varchar(20))")
     }
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    fun insertUserData( name:String,email:String,password:String){
+    fun insertUserData( name:String,password:String){
         val db: SQLiteDatabase = writableDatabase
         val values: ContentValues = ContentValues()
         values.put("name",name)
-        values.put("email",email)
         values.put("password",password)
 
         db.insert("user",null, values)
         db.close()
     }
 
-    fun userPresent(email: String, password: String):Boolean{
+    fun userPresent(name: String, password: String):Boolean{
         val db=writableDatabase
-        val query="select * from user where email = '$email' and password = '$password'"
+        val query="select * from user where name = '$name' and password = '$password'"
         val cursor=db.rawQuery(query, null)
         if (cursor.count <= 0){
             cursor.close()
--- app/src/main/java/edu/towson/cosc431/christian/hangman/MainActivity.kt
+++ app/src/main/java/edu/towson/cosc431/christian/hangman/MainActivity.kt
@@ -8,9 +8,12 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.background.*
 import kotlinx.android.synthetic.main.choice.*
+import kotlinx.android.synthetic.main.game_layout.*
 import kotlinx.android.synthetic.main.login.*
 import kotlinx.android.synthetic.main.settings.*
 import kotlinx.android.synthetic.main.user_registration.*
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,11 +34,11 @@ class MainActivity : AppCompatActivity() {
             showLogin()
         }
         save.setOnClickListener{
-            handler.insertUserData(name.text.toString(),email.text.toString(),password_registration.text.toString())
+            handler.insertUserData(name.text.toString(),password_registration.text.toString())
             showHome()
         }
         login_button.setOnClickListener {
-            if (handler.userPresent(login_email.text.toString(), login_password.text.toString())){
+            if (handler.userPresent(login_name.text.toString(), login_password.text.toString())){
                 Toast.makeText(this,"Login Success",Toast.LENGTH_SHORT).show()
                 showChoice()
             }
@@ -85,7 +88,33 @@ class MainActivity : AppCompatActivity() {
             showChoice()
 
         }
+        single_btn.setOnClickListener {
+            showgame()
+            fetchjson()
+        }
 
+    }
+
+    private fun fetchjson(){
+        println("hey it works")
+        word.text = "hey"
+        val url = "https://www.wordgamedictionary.com/word-lists/5-letter-words/5-letter-words.json"
+        val request = Request.Builder().url(url).build()
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                println("failed try the fuck again")
+            }
+
+            override fun onResponse(call: Call?, response: Response?) {
+                val body = response?.body()?.string()
+                println(body)
+
+
+
+
+            }
+        })
     }
 
     private fun showBackground() {
@@ -97,8 +126,19 @@ class MainActivity : AppCompatActivity() {
         background_layout.visibility=View.VISIBLE
     }
 
+    private fun showgame(){
+        game_layout.visibility = View.VISIBLE
+        registration_layout.visibility=View.GONE
+        login_layout.visibility=View.GONE
+        home_ll.visibility=View.GONE
+        choice_layout.visibility=View.GONE
+        settings_layout.visibility=View.GONE
+        background_layout.visibility=View.GONE
+    }
+
 
     private fun showRegistration(){
+        game_layout.visibility = View.GONE
         registration_layout.visibility=View.VISIBLE
         login_layout.visibility=View.GONE
         home_ll.visibility=View.GONE
@@ -107,6 +147,7 @@ class MainActivity : AppCompatActivity() {
         background_layout.visibility=View.GONE
     }
     private fun showLogin(){
+        game_layout.visibility = View.GONE
         registration_layout.visibility=View.GONE
         login_layout.visibility=View.VISIBLE
         home_ll.visibility=View.GONE
@@ -115,6 +156,7 @@ class MainActivity : AppCompatActivity() {
         background_layout.visibility=View.GONE
     }
     private fun showHome(){
+        game_layout.visibility = View.GONE
         registration_layout.visibility=View.GONE
         login_layout.visibility=View.GONE
         home_ll.visibility=View.VISIBLE
@@ -123,6 +165,7 @@ class MainActivity : AppCompatActivity() {
         background_layout.visibility=View.GONE
     }
     private fun showChoice(){
+        game_layout.visibility = View.GONE
         registration_layout.visibility=View.GONE
         login_layout.visibility=View.GONE
         home_ll.visibility=View.GONE
@@ -131,6 +174,7 @@ class MainActivity : AppCompatActivity() {
         background_layout.visibility=View.GONE
     }
     private fun showSettings(){
+        game_layout.visibility = View.GONE
         registration_layout.visibility=View.GONE
         login_layout.visibility=View.GONE
         home_ll.visibility=View.GONE
